3d5d40c4ab9a90bac108087fd75d1460c92e12cf
per6 <kyroschow@gmail.com>
2017-09-29 13:10:40 -0700

    When I was younger I used to fight lava sharks and agravated pigmen.

--- app/src/main/java/com/example/chow/listexample/DisplayActivity.java
+++ app/src/main/java/com/example/chow/listexample/DisplayActivity.java
@@ -3,11 +3,13 @@ package com.example.chow.listexample;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
 
     private TextView heroName, description;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,5 +29,6 @@ public class DisplayActivity extends AppCompatActivity {
     private void wireWidgets() {
         heroName = (TextView) findViewById(R.id.name_hero);
         description = (TextView) findViewById(R.id.description);
+        image = (ImageView) findViewById(R.id.image);
     }
 }
